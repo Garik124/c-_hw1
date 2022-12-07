9e8d9bad6db27600286fba41c73961624ec6cdde
Garik <requie@mail.ru>
2022-12-07 16:57:56 +0700

    task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿int N = int.Parse(Console.ReadLine());
+int current;
+for (current = 1; current < N + 1; current++)
+{
+    if (current % 2 == 0);
+    {
+        Console.WriteLine(current + " ");
+    }
+}
