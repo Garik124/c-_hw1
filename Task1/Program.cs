33a787377833c04ff7484648a1c3f0834c3cb62d
Garik <requie@mail.ru>
2022-12-07 15:19:59 +0700

    task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+
+if (A > B)
+{
+    Console.Write($"{A} - max, {B} - min");
+}
+
+else if (A < B)
+{
+    Console.Write($"{B} - max, {A} - min");
+}
